384c51daad99317d0c6ef2e6f7b8671ffa964467
bullno1 <thebullno1@gmail.com>
2014-10-08 14:56:16 +0800

    Update active-app and library-app to use pluggable .mk files

--- shared/rebar.mk
+++ shared/rebar.mk
@@ -1,21 +1,17 @@
-PLT_APPS ?= erts kernel stdlib
-DIALYZER_OPTS ?= -Werror_handling -Wrace_conditions -Wunmatched_returns
+PLT_APPS?=erts kernel stdlib
+DIALYZER_OPTS?=-Werror_handling -Wrace_conditions -Wunmatched_returns -Wunderspecs
 
-REBAR ?= $(CURDIR)/rebar
-REBAR_URL ?= https://github.com/rebar/rebar/releases/download/2.5.1/rebar
+REBAR?=$(CURDIR)/rebar
+REBAR_URL?=https://github.com/rebar/rebar/releases/download/2.5.1/rebar
 
-RELX ?= $(CURDIR)/relx
-RELX_URL ?= https://github.com/erlware/relx/releases/download/v1.0.4/relx
+RELX?=$(CURDIR)/relx
+RELX_URL?=https://github.com/erlware/relx/releases/download/v1.0.4/relx
 
-ifdef ERL_LIBS
-RELX_OPTS?=--lib-dir $(ERL_LIBS)
-else
 RELX_OPTS?=
-endif
 
 PROJECT_PLT=$(CURDIR)/.project_plt
 
-ERL = $(shell which erl)
+ERL=$(shell which erl)
 ifeq ($(ERL),)
 $(error "Erlang not available on this system")
 endif
@@ -23,33 +19,38 @@ endif
 export REBAR
 export RELX
 
-.PHONY: all compile clean test dialyzer typer distclean \
-        get-deps clean-common-test-data release
+.PHONY: rebar-all rebar-compile rebar-clean rebar-test \
+        rebar-get-deps rebar-release dialyzer
 
-all: compile
+DEFAULT+=rebar-all
+CLEAN+=rebar-clean
+TEST+=rebar-test
+GET_DEPS+=rebar-get-deps
+RELEASE+=rebar-release
+TEST+=rebar-test
 
-get-deps: $(REBAR)
+rebar-all: rebar-compile
+
+rebar-get-deps: $(REBAR) .build/erlang_deps
+
+.build/erlang_deps: rebar.config
 	$(REBAR) get-deps
+	@mkdir -p .build
+	@touch .build/erlang_deps
+
+rebar-compile: rebar-get-deps $(REBAR)
 	$(REBAR) compile
 
-compile: $(REBAR)
-	$(REBAR) skip_deps=true compile
-
-release: compile $(RELX)
+rebar-release: rebar-compile $(RELX)
 	$(RELX) $(RELX_OPTS)
-	rm -rf ./_rel/*/erts-*/include
-	rm -rf ./_rel/*/erts-*/lib
-	rm -rf ./_rel/*/erts-*/src
-	rm -rf ./_rel/*/erts-*/doc
-	rm -rf ./_rel/*/erts-*/man
 
-eunit: compile clean-common-test-data
+eunit: rebar-compile clean-common-test-data
 	$(REBAR) skip_deps=true eunit
 
-ct: compile clean-common-test-data
+ct: rebar-compile clean-common-test-data
 	$(REBAR) skip_deps=true ct
 
-test: compile eunit ct
+rebar-test: rebar-compile eunit ct
 
 define download
 	wget -O $(1) $(2) || rm $(1)
@@ -66,27 +67,13 @@ $(PROJECT_PLT):
 	@echo Building local plt at $(PROJECT_PLT)
 	@echo
 	dialyzer --output_plt $(PROJECT_PLT) --build_plt \
-	         --apps $(PLT_OTP_APPS) -r deps
+	         --apps $(PLT_APPS) -r deps
 
-dialyzer: compile $(PROJECT_PLT)
+dialyzer: rebar-compile $(PROJECT_PLT)
 	dialyzer --plt $(PROJECT_PLT) \
 	         --fullpath \
-	         $(DIALYZER_FLAGS) \
-	         -I $(CURDIR)/deps -pa $(CURDIR)/ebin --src src
+	         $(DIALYZER_OPTS) \
+	         --src -r src
 
-typer: compile
-	typer --plt $(PROJECT_PLT) -r ./src
-
-clean-common-test-data:
-# We have to do this because of the unique way we generate test
-# data. Without this rebar eunit gets very confused
-	- rm -rf $(CURDIR)/test/*_SUITE_data
-
-clean: clean-common-test-data
-	- rm -rf $(CURDIR)/test/*.beam
-	- rm -rf $(CURDIR)/logs
-	$(REBAR) skip_deps=true clean
-
-distclean: clean
-	- rm -rf $(PROJECT_PLT)
-	- rm -rvf $(CURDIR)/deps/*
+rebar-clean:
+	$(REBAR) clean
--- shared/shell.sh
+++ shared/shell.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-make
 APPDIR=`dirname "$(readlink -f "$0")"`
-if [[ $? == 0 ]]; then
-	erl +K true \
-	    -pa $APPDIR/ebin $APPDIR/deps/*/ebin \
-        -boot start_sasl $*
-fi
+make &&
+erl +K true \
+    +W w \
+    -pa $APPDIR/ebin $APPDIR/deps/*/ebin \
+    -boot start_sasl $*
